c6fda81088e9436ee603ea2e84505c98fc98c305
taiki365 <akahoshihit0915tigers2003v@yahoo.co.jp>
2021-10-20 17:40:45 +0900

    チャットルームの削除機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,5 @@
 class MessagesController < ApplicationController
+
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
@@ -15,7 +16,6 @@ class MessagesController < ApplicationController
       render :index
     end
 end
-
 
 private
 
